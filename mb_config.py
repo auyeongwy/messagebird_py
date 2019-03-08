7975d4e8af5328ca078bdb7944d72fb8c1ee0fd4
Au Yeong Wing Yau <auyeongwy@gmail.com>
2019-03-08 16:39:04 +0800

    Implemented GET filters. Now able to perform a GET query using any number of filters.

--- mb_config.py
+++ mb_config.py
@@ -17,36 +17,90 @@
 # -*- coding: utf-8 -*-
 
 
-"""
-Manages reading the config file parameters into variables.
-Usage:
-
-import mb_config
-
-try:
-	my_config = mb_config.mb_config("configfile")
-except Exception as except:
-	print(except.args[0])
-	print("Abort")
-	sys.exit()
-	
-print(vars(my_config))
-"""
 
 import configparser
 
 class mb_config:
+	"""
+	Manages reading the config file parameters into variables.
+	Usage:
+
+	import mb_config
+
+	try:
+	my_config = mb_config.mb_config("configfile")
+	except Exception as except:
+		print(except.args[0])
+		print("Abort")
+		sys.exit()
 	
-	def __init__(self, p_file):
-		self.config = configparser.ConfigParser()
-		if len(self.config.read(p_file)) == 0:
-			raise Exception("Config file error.")
+	print(vars(my_config))
+	"""
+
+	def read_message_config(self, p_config):
+		""" 
+		Reads config options from the "Message" part of the config file into the current object's variables.
+		:param p_config: The configparser.ConfigParser object that has opened the config file. 
+		"""
+		if "OA" in p_config["Message"]:
+			self.oa = p_config["Message"]["OA"]
+		if "Recipient" in p_config["Message"]:
+			self.recipient = p_config["Message"]["Recipient"]
+		if "Message" in p_config["Message"]:
+			self.message = p_config["Message"]["Message"]		
+
+		
+
+	def read_filter_config(self, p_config):
+		""" 
+		Reads config options from the "Filter" part of the config file into the current object's variables.
+		:param p_config: The configparser.ConfigParser object that has opened the config file. 
+		"""		
+		if "enabled" in p_config["Filter"]:
+			self.filter_enabled = p_config["Filter"]["enabled"]
+			if self.filter_enabled == '1':
+				if "originator" in p_config["Filter"]:
+					self.filter_originator = p_config["Filter"]["originator"]
+				if "recipient" in p_config["Filter"]:
+					self.filter_recipient = p_config["Filter"]["recipient"]
+				if "direction" in p_config["Filter"]:
+					self.filter_direction = p_config["Filter"]["direction"]
+				if "limit" in p_config["Filter"]:
+					self.filter_limit = p_config["Filter"]["limit"]
+				if "offset" in p_config["Filter"]:
+					self.filter_offset = p_config["Filter"]["offset"]
+				if "searchterm" in p_config["Filter"]:
+					self.filter_searchterm = p_config["Filter"]["searchterm"]
+				if "type" in p_config["Filter"]:
+					self.filter_type = p_config["Filter"]["type"]
+				if "contact_id" in p_config["Filter"]:
+					self.filter_recipient = p_config["Filter"]["contact_id"]
+				if "status" in p_config["Filter"]:
+					self.filter_status = p_config["Filter"]["status"]
+				if "from" in p_config["Filter"]:
+					self.filter_from = p_config["Filter"]["from"]
+				if "until" in p_config["Filter"]:
+					self.filter_until = p_config["Filter"]["until"]
 		else:
-			self.sms_api_key = self.config["Message Bird"]["SMS_API_KEY"]
-			self.oa = self.config["Message"]["OA"]
-			self.recipient = self.config["Message"]["Recipient"]
-			self.message = self.config["Message"]["Message"]
+			self.filter_enabled = '0'
+
+		
+		
+	def __init__(self, p_file):
+		""" 
+		Reads config options from a configuration file into this object.
+		:param p_file: The configuration file. 
+		"""
+		config = configparser.ConfigParser()
+		if len(config.read(p_file)) == 0:
+			raise Exception("Config file error.")
+
+		if "SMS_API_KEY" in config["Key"]:
+			self.sms_api_key = config["Key"]["SMS_API_KEY"]
+		else:
+			"""If API key is missing just give up."""
+			raise Exception("Missing API key.") 
 			
-			if len(self.config["Message Bird"]["SMS_API_KEY"]) == 0:
-				raise Exception("Missing API Key")
+		self.read_message_config(config)
+		self.read_filter_config(config)
 		
--- mb_sms.py
+++ mb_sms.py
@@ -24,28 +24,109 @@ python3 mb_sms.py
 """
 
 import mb_config
-import sys, http.client, json
+import sys 
+import http.client 
+import json
+import urllib
 
+
+def create_url(p_config):
+	"""
+	Constructs the URL for the REST request
+	:param p_config: An mb_config.mb_config object that contains the configuration parameters
+	"""
+	url = '/messages'
+	params = {}
+	if p_config.filter_enabled == '1':
+		if hasattr(p_config, 'filter_originator'):
+			params['originator'] = p_config.filter_originator
+		if hasattr(p_config, 'filter_recipient'):
+			params['recipient'] = p_config.filter_recipient
+		if hasattr(p_config, 'filter_direction'):
+			params['direction'] = p_config.filter_direction
+		if hasattr(p_config, 'filter_limit'):
+			params['limit'] = p_config.filter_limit
+		if hasattr(p_config, 'filter_offset'):
+			params['offset'] = p_config.filter_offset
+		if hasattr(p_config, 'filter_searchterm'):
+			params['searchterm'] = p_config.filter_searchterm
+		if hasattr(p_config, 'filter_type'):
+			params['type'] = p_config.filter_type
+		if hasattr(p_config, 'filter_contact_id'):
+			params['contact_id'] = p_config.filter_contact_id
+		if hasattr(p_config, 'filter_status'):
+			params['status'] = p_config.filter_status
+		if hasattr(p_config, 'filter_from'):
+			params['from'] = p_config.filter_from
+		if hasattr(p_config, 'filter_until'):
+			params['until'] = p_config.filter_until
+		
+	if len(params) > 0:
+		url = url+'?'+urllib.parse.urlencode(params)
+	return url
+	
+	
+	
+def create_payload(p_config):
+	"""
+	Constructs the payload for the REST request
+	:param p_config: An mb_config.mb_config object that contains the configuration parameters
+	"""	
+	payload = {}
+	if p_config.filter_enabled == '1':
+		if hasattr(p_config, 'filter_originator'):
+			payload['originator'] = p_config.filter_originator
+	
+	if len(payload) > 0:
+		payload = json.dumps(payload)
+		
+	return payload
+	
+	
+
+def create_header(p_config):
+	"""
+	Constructs the header for the REST request
+	:param p_config: An mb_config.mb_config object that contains the configuration parameters
+	"""		
+	header = {}
+	header['Authorization'] = 'AccessKey '+p_config.sms_api_key
+	return header
+	
+	
+
+""" Load the config file """
 try:
 	config = mb_config.mb_config("config.real")
 except Exception as excep:
 	print(excep.args[0])
 	sys.exit()
 
-api_key_header = 'AccessKey '+config.sms_api_key
+""" Get the access key """
+v_payload = create_payload(config)
 #print(vars(config))
-	
+
+
+
 v_client_conn = http.client.HTTPSConnection('rest.messagebird.com')
+v_url = create_url(config)
+#print(v_url)
+	
 try:
-	v_client_conn.request("GET","/messages", None, {'Authorization':api_key_header})
+	v_client_conn.request("GET", v_url, None, create_header(config))
 	response = v_client_conn.getresponse()
 except http.client.HTTPException as he:
 	print(he.args[0])
 	sys.exit
+
+
+""" Work with the results """
+print(response.status, response.reason)
+if response.status != 200: 
+	print(response.read())
 	
-print(response.status)
-print(response.reason)
-v_result = (response.read()).decode('utf-8')
-print(json.dumps(json.loads(v_result), indent=2, sort_keys=True))
+else:
+	v_result = (response.read()).decode('utf-8')
+	print(json.dumps(json.loads(v_result), indent=2, sort_keys=True))
 
 v_client_conn.close()
